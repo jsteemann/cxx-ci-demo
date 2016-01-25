c34f3745b7a398b8522d7f7b4a37e7b5fcce06dc
jsteemann <jan@arangodb.com>
2016-01-25 23:03:23 +0100

    attempt to automatically get a backtrace

--- scripts/after-failure.sh
+++ scripts/after-failure.sh
@@ -1,5 +1,6 @@
+cd build/tests
 COREFILE=$(find . -maxdepth 1 -name "core*" | head -n 1)
 if [[ -f "$COREFILE" ]]; then 
-  gdb -c "$COREFILE" bin/arangod -ex "thread apply all bt" -ex "set pagination 0" -batch || echo "no gdb found" 
+  gdb -c "$COREFILE" -ex "thread apply all bt" -ex "set pagination 0" -batch || echo "no gdb found" 
 fi 
 
