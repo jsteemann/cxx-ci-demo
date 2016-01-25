d2919eeb2719b846c2b2227ff5fea8a44391f57e
jsteemann <jan@arangodb.com>
2016-01-25 23:11:33 +0100

    debugging

--- scripts/after-failure.sh
+++ scripts/after-failure.sh
@@ -1,6 +1,12 @@
+echo "after-failure..."
+ulimit -a # print ulimits
+
 cd build/tests
 COREFILE=$(find . -maxdepth 1 -name "core*" | head -n 1)
 if [[ -f "$COREFILE" ]]; then 
+  echo "core file found: $COREFILE"
   gdb -c "$COREFILE" -ex "thread apply all bt" -ex "set pagination 0" -batch || echo "no gdb found" 
+else
+  echo "no core file found"
 fi 
 
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,12 @@
+echo "adjusting ulimits"
 ulimit -c unlimited -S # enable core files
+ulimit -a # print limits
+
 mkdir -p build # create build directory if it does not exists
+
+echo "building..."
 (cd build; cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=$CXX .. && make) # make
+
+echo "running tests..."
 (cd build/tests && ctest -V) # run tests
 
